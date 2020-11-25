0511f71d9b769adbf056bd561a325af32e361fe2
Michael <michaelritter@gmx.net>
2020-11-25 11:25:34 +0100

    Initial release of current project state.

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,15 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"sap/ui/demo/walkthrough/test/integration/NavigationJourney"
+	], 
+		function () {
+			QUnit.start();
+		}
+	);
+});
